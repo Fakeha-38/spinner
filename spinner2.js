11e0f40d43938b3fdb6676a5f2ef6dd71613ec24
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-04-20 16:01:44 -0400

    spinner 1 and 2 using async code

---
+++ spinner2.js
@@ -0,0 +1,6 @@
+let spinnerArr = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
+for (let i = 0; i < spinnerArr.length; i++) {
+  setTimeout(() => {
+    process.stdout.write("\r" + spinnerArr[i] + ' ');
+  }, i * 200);
+}
